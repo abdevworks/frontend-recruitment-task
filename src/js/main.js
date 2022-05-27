11271703deca4779c1d7fba72e98022bb04eb094
ABDev <czasacz@gmail.com>
2022-05-27 12:25:58 +0200

    Final Project Version.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+const sectionTemp = document.querySelector(".sectionTemplate");
+const wrapper = document.querySelector(".wrapper");
 const sectionButton = document.querySelector("#section-button");
 const alertCloseButton = document.querySelector("#alert-closeButton");
 const alertResetButton = document.querySelector("#alert-resetButton");
@@ -5,49 +7,64 @@ const alertMessageCounter = document.querySelector("#alert-messageCounter");
 const alert = document.querySelector(".alert");
 const alertShadow = document.querySelector(".alert__shadow");
 
-sectionButton.addEventListener("click", () => {
-  toggleAlert();
-});
-alertShadow.addEventListener("click", () => {
-  toggleAlert();
-});
-alertCloseButton.addEventListener("click", () => {
-  toggleAlert();
-});
-alertResetButton.addEventListener("click", () => {
-  resetCounter();
-});
+function addMoreSections(section, sectionId) {
+  const sectionButton = section.querySelector("#section-button");
+  const alertCloseButton = section.querySelector("#alert-closeButton");
+  const alertResetButton = section.querySelector("#alert-resetButton");
+  const alertMessageCounter = section.querySelector("#alert-messageCounter");
+  const alert = section.querySelector(".alert");
+  const alertShadow = section.querySelector(".alert__shadow");
 
-//Set initial state of alert
-let showAlert = false;
-let showReset = false;
-let alertClickCounter = 0;
-alertClickCounter = sessionStorage.getItem("alertClickCounter");
+  sectionButton.addEventListener("click", () => {
+    toggleAlert();
+  });
+  alertShadow.addEventListener("click", () => {
+    toggleAlert();
+  });
+  alertCloseButton.addEventListener("click", () => {
+    toggleAlert();
+  });
+  alertResetButton.addEventListener("click", () => {
+    resetCounter();
+  });
 
-function toggleAlert() {
-  if (!showAlert) {
-    alert.classList.add("show");
-    alertShadow.classList.add("show");
-    alertClickCounter++;
-    alertMessageCounter.textContent = `${alertClickCounter} times`;
-    sessionStorage.setItem("alertClickCounter", alertClickCounter);
-    showAlert = true;
+  // Set initial state of alert
+  let showAlert = false;
+  let showReset = false;
+  let alertClickCounter = 0;
+  alertClickCounter = sessionStorage.getItem(`${sectionId}+alertClickCounter`);
 
-    if (alertClickCounter >= 5) {
-      alertResetButton.classList.add("show");
+  function toggleAlert() {
+    if (!showAlert) {
+      alert.classList.add("show");
+      alertShadow.classList.add("show");
+      alertClickCounter++;
+      alertMessageCounter.textContent = `${alertClickCounter} times`;
+      sessionStorage.setItem(`${sectionId}+alertClickCounter`, alertClickCounter);
+      showAlert = true;
+
+      if (alertClickCounter >= 5) {
+        alertResetButton.classList.add("show");
+      } else {
+        alertResetButton.classList.remove("show");
+      }
     } else {
+      alert.classList.remove("show");
+      alertShadow.classList.remove("show");
       alertResetButton.classList.remove("show");
+      showAlert = false;
     }
-  } else {
-    alert.classList.remove("show");
-    alertShadow.classList.remove("show");
-    alertResetButton.classList.remove("show");
-    showAlert = false;
   }
+
+  function resetCounter() {
+    alertClickCounter = 0;
+    sessionStorage.setItem(`${sectionId}+alertClickCounter`, alertClickCounter);
+    toggleAlert();
+  }
+  wrapper.appendChild(section);
 }
 
-function resetCounter() {
-  alertClickCounter = 0;
-  sessionStorage.setItem("alertClickCounter", alertClickCounter);
-  toggleAlert();
-}
+const section = sectionTemp.content.cloneNode(true);
+addMoreSections(section,"sectionOne");
+// const sectionTwo = sectionTemp.content.cloneNode(true);
+// addMoreSections(sectionTwo, "sectionTwo");
